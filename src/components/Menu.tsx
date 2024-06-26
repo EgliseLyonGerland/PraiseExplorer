349265d06e5e3d61b48beca59a87871668e8b6ba
Nicolas Bazille <oltodo@msn.com>
2024-06-26 20:23:39 +0200

    Add more responsiveness

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,8 +1,6 @@
 import { useMemo, useState } from "react";
-import Logo from "./Logo";
 import MiniSearch from "minisearch";
 import type { Song } from "@/types";
-import { MoonIcon, SunIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 
 interface Props {
@@ -35,24 +33,7 @@ export default function Menu({ data, currentSongId }: Props) {
   }, [data]);
 
   return (
-    <div className="menu bg-base-200 text-base-content min-h-full p-8 w-[512px]">
-      <div className="flex items-center gap-4">
-        <Logo className="h-10 fill-base-content" />
-        <span className="flex gap-2 items-center">
-          <span className="font-serif font-bold text-xl leading-none">
-            Chants
-          </span>
-          <span className="badge badge-primary badge-sm mb-4">Beta</span>
-        </span>
-
-        <label className="swap swap-rotate ml-auto">
-          <input type="checkbox" className="theme-controller" value="dark" />
-
-          <SunIcon className="swap-off size-6 fill-current" />
-          <MoonIcon className="swap-on size-6 fill-current" />
-        </label>
-      </div>
-
+    <>
       <div className="py-8 sticky top-0 bg-base-200 border-b border-base-300 mb-4 z-10">
         <input
           id="search"
@@ -78,15 +59,15 @@ export default function Menu({ data, currentSongId }: Props) {
 
       <ul>
         {songs.map((song) => (
-          <li key={song.id} className="min-w-96">
+          <li key={song.id} className="md:min-w-96">
             <a
               className={clsx(
-                "flex flex-col items-start gap-0 px-8",
+                "flex flex-col items-start gap-0 px-6 sm:px-8",
                 currentSongId === song.id && "active"
               )}
               href={`/${song.id}`}
             >
-              <span className="text-lg font-serif line-clamp-1">
+              <span className="sm:text-lg font-serif line-clamp-1">
                 {song.title}
               </span>
 
@@ -97,6 +78,6 @@ export default function Menu({ data, currentSongId }: Props) {
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 }
